5ca1c0e4e46ccb1ab83a72396c720501886ab34b
Justin LeBlanc <leblanc@wustl.edu>
2023-05-08 15:19:45 +0100

    Fill in the view for entering a prompt.

--- LanguageBuddy/Views/PromptEntryView.swift
+++ LanguageBuddy/Views/PromptEntryView.swift
@@ -1,8 +1,19 @@
 import SwiftUI
 
 struct PromptEntryView: View {
+    @State private var prompt: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .trailing) {
+            TextField("Your question here...", text: $prompt)
+                .frame(maxWidth: .infinity)
+                .border(Color.gray, width: 2)
+                .cornerRadius(4)
+            Image(systemName: "paperplane")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+                .padding(.trailing, 10)
+        }
+        .padding(10)
     }
 }
 

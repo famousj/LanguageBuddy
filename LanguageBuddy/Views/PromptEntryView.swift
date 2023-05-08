aef4f3d6bfe9525c1b98a60d5397d051b348da1f
Justin LeBlanc <leblanc@wustl.edu>
2023-05-08 13:44:28 +0100

    Make a new view for entering a prompt

---
+++ LanguageBuddy/Views/PromptEntryView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PromptEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PromptEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        PromptEntryView()
+    }
+}

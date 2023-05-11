0c93fdc7b73e4d0014d2476e8933ca0d8ba564c0
Justin LeBlanc <leblanc@wustl.edu>
2023-05-11 10:06:25 +0100

    Fix a test

--- LanguageBuddyTests/MessageCreatorTests.swift
+++ LanguageBuddyTests/MessageCreatorTests.swift
@@ -12,7 +12,7 @@ final class MessageCreatorTests: XCTestCase {
         XCTAssertEqual(messages.count, 2)
         
         let preface = messages.first
-        let expectedPreface = Message(role: .user, content: "This question is about \(language)")
+        let expectedPreface = Message(role: .user, content: "This question is about \(language).")
         XCTAssert(preface!.isEqualTo(expectedPreface))
         
         let message = messages.last
